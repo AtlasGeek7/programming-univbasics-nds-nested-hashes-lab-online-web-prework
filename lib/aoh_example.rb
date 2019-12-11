20938b049426fbbe6067ed6ab7683df761a7e7aa
Khalid Tissire <ghettoguy7@gmail.com>
2019-12-11 00:07:47 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,20 +18,10 @@ def literal_aoh
   people = [
     {:name=>"Don Gately", :occupation=>"Live-in Staff"},
     {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
-    {:name=>"Pat Monteseian",
-    :occupation=>"Staff"},
-    {:name=>"Kate Gompert",
-    :occupation=>"None"}, 
-    {:name=>"Bruce Green",
-    :occupation=>"Fan of Mildred"}
-    ]
-    
-    
-    film_fest = [
-  {:name=>"Ida Lupino", :favorite_movie=>"The Hitch-Hiker"},
-  {:name=>"Orson Welles", :favorite_movie=>"Citizen Kane"},
-  {:name=>"Andrej Tarkovsky", :favorite_movie=>"Solaris"}
-]
+    {:name=>"Pat Monteseian", :occupation=>"Staff"},
+    {:name=>"Kate Gompert", :occupation=>"None"}, 
+    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
